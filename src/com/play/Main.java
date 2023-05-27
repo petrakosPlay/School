072c8d2ced46e4f9bdc32995263967e8360e7d8b
petrakos <contactpetrosk@gmail.com>
2023-05-27 18:49:36 +0300

    Modified reading the Students' file in Main

--- src/com/play/Main.java
+++ src/com/play/Main.java
@@ -15,46 +15,61 @@ public class Main
             return;
         }
         
+
         
-        School school = new School(Short.parseShort(args[0]), Short.parseShort(args[1]),
-                                   Short.parseShort(args[2]), Short.parseShort(args[3]));
+        School school = new School(Short.parseShort(args[0]), Short.parseShort(args[1]), Short.parseShort(args[2]), Short.parseShort(args[3]));
+        
+
+        File file;
+        FileReader fReader;
+        BufferedReader bReader;
+        Student student;
 
         try {
-            //File file = new File(inputFileName);
-            File file = new File(".\\Student_data.csv");
-            FileReader fReader = new FileReader(file);
-            BufferedReader bReader = new BufferedReader(fReader);
-            String[] studentData;
-            Student student;
-            int i =0;
-            for (String line = bReader.readLine(); i < 9; line=bReader.readLine())
-            //for (String line = bReader.readLine(); line!=null; line=bReader.readLine())
-            {
-                studentData = line.split(",", 0);
-                student = new Student(studentData[0], Short.parseShort(studentData[1]), Short.parseShort(studentData[2]));
-                student.showInfo();
-                school.enter(student);
-                ++i;
-            }
+            file = new File(".\\Student_data.csv");
+            fReader = new FileReader(file);
+            bReader = new BufferedReader(fReader);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        
+        student = getNextStudent(bReader);
+        school.enter(student);
+
+
+        try {
             bReader.close();
             fReader.close();
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             e.printStackTrace();
+            return;
         }
 
+        /* 
         Teacher[] teachers = new Teacher[6];
         for(short i=0; i<6; ++i)
         {
             teachers[i] = new Teacher("Teacher_" + Integer.toString(i+1), (short)(i%3), (short) (i+1));
             teachers[i].showInfo();
-        }
+        }*/
     }
 
-    void openFile(String inputFileName)
+
+    static Student getNextStudent(BufferedReader bReader)
     {
-        
+        Student student = null;
+        try {
+            String[] studentData = bReader.readLine().split(",", 0);
+            student = new Student(studentData[0], Short.parseShort(studentData[1]), Short.parseShort(studentData[2]));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return student;
     }
 }
 
